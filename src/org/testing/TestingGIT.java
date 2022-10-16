50bc3fa0f16cd6cda66a9f1112e35becd1cbd56e
Amarsimbu <amarirfan007@gmail.com>
2022-10-16 20:14:09 +0530

    FinalScript

--- src/org/testing/TestingGIT.java
+++ src/org/testing/TestingGIT.java
@@ -18,13 +18,12 @@ public class TestingGIT {
 		} else {
             System.out.println("Given num is nonPrime number");
 		}
-<<<<<<< HEAD
-		System.out.println("Prime number is 2 3 5 7 ");
-		System.out.println("Non Prime number is 4 6 8 10");
-=======
 		
 		System.out.println("Print the prime number");
 		System.out.println("Print the non prime number");
->>>>>>> 89569085c6ce4395b6486b8aa591b58e3fe22a32
+		
+		System.out.println("Prime number is 2 3 5 7 ");
+		System.out.println("Non Prime number is 4 6 8 10");
+
 	}
 }
